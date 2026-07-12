a7e2ceda1a998bcbb72291b893c1bee88d40909d
Atanas Ladzhov <ladjo@gbg.bg>
2026-07-12 11:03:29 +0300

    fix main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,24 @@
 import { Routes } from '@angular/router';
-import {InvoiceScanner} from './invoice-scanner/invoice-scanner';
+import { InvoiceScanner } from './invoice-scanner/invoice-scanner';
+import { AppMain } from './app-main/app-main';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'winery', pathMatch: 'full' },
-  { path: 'winery', data: { tab: 'winery' }, children: [] },
-  { path: 'history', data: { tab: 'family' }, children: [] },
-  { path: 'vineyards', data: { tab: 'vineyards' }, children: [] },
-  { path: 'wines', data: { tab: 'wines' }, children: [] },
-  { path: 'wines/:slug', data: { tab: 'wines' }, children: [] },
-  { path: 'checkout', data: { tab: 'checkout' }, children: [] },
-  { path: 'terms', data: { tab: 'terms' }, children: [] },
-  { path: 'privacy', data: { tab: 'privacy' }, children: [] },
-  { path: 'delivery-info', data: { tab: 'delivery-info' }, children: [] },
   { path: 'invoice-scanner', component: InvoiceScanner },
-  { path: '**', redirectTo: 'winery' },
+  {
+    path: '',
+    component: AppMain,
+    children: [
+      { path: '', redirectTo: 'winery', pathMatch: 'full' },
+      { path: 'winery', data: { tab: 'winery' }, children: [] },
+      { path: 'history', data: { tab: 'family' }, children: [] },
+      { path: 'vineyards', data: { tab: 'vineyards' }, children: [] },
+      { path: 'wines', data: { tab: 'wines' }, children: [] },
+      { path: 'wines/:slug', data: { tab: 'wines' }, children: [] },
+      { path: 'checkout', data: { tab: 'checkout' }, children: [] },
+      { path: 'terms', data: { tab: 'terms' }, children: [] },
+      { path: 'privacy', data: { tab: 'privacy' }, children: [] },
+      { path: 'delivery-info', data: { tab: 'delivery-info' }, children: [] },
+    ]
+  },
+  { path: '**', redirectTo: '' },
 ];
